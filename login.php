db125353243e72b6f6f1b04e6ab5bc5f1570d7f7
fedelmutungiKE <mutungifedel@gmail.com>
2025-08-10 01:26:28 +0300

    Add files via upload

---
+++ login.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Fedel Real Estate Login</title>
+</head>
+<body>
+  <h2>Customer Login</h2>
+  <form action="login.php" method="post">
+    <label>Username:</label><br>
+    <input type="text" name="username" required><br><br>
+
+    <label>Password:</label><br>
+    <input type="password" name="password" required><br><br>
+
+    <label>ID Number:</label><br>
+    <input type="text" name="idnumber" required><br><br>
+
+    <label>Phone Number:</label><br>
+    <input type="text" name="phone" required><br><br>
+
+    <input type="submit" value="Login">
+  </form>
+</body>
+</html>
